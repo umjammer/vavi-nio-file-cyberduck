a7ff90471b2c5ebc8c408b98ad8a6296a052e1cd
umjammer <umjammer@gmail.com>
2020-06-05 19:07:50 +0900

    remove redundancies

--- src/test/java/vavi/nio/file/cyberduck/Main4.java
+++ src/test/java/vavi/nio/file/cyberduck/Main4.java
@@ -10,26 +10,15 @@ import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import vavi.net.fuse.Fuse;
-import vavi.util.Debug;
-
-import vavix.util.Checksum;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import vavi.net.fuse.Base;
 
 
 /**
@@ -41,16 +30,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisabledIfEnvironmentVariable(named = "GITHUB_WORKFLOW", matches = ".*")
 public class Main4 {
 
-    static Fuse fuse;
-    static String mountPoint;
-    static FileSystem fs;
+    String mountPoint;
+    FileSystem fs;
+    Map<String, Object> options;
 
-    @BeforeAll
-    public static void before() throws Exception {
+    @BeforeEach
+    public void before() throws Exception {
         System.setProperty("vavi.util.logging.VaviFormatter.extraClassMethod", "co\\.paralleluniverse\\.fuse\\.LoggedFuseFilesystem#log");
-
-        System.setProperty("vavi.net.fuse.FuseProvider.class", "vavi.net.fuse.javafs.JavaFSFuseProvider");
-//        System.setProperty("vavi.net.fuse.FuseProvider.class", "vavi.net.fuse.jnrfuse.JnrFuseFuseProvider");
 
         mountPoint = System.getenv("TEST4_MOUNT_POINT");
         String username = URLEncoder.encode(System.getenv("TEST4_SFTP_ACCOUNT"), "utf-8");
@@ -66,116 +52,26 @@ public class Main4 {
 
         fs = FileSystems.newFileSystem(uri, env);
 
-        Map<String, Object> options = new HashMap<>();
-        options.put("fsname", "googledrive_fs" + "@" + System.currentTimeMillis());
+        options = new HashMap<>();
+        options.put("fsname", "cyberduck_fs" + "@" + System.currentTimeMillis());
         options.put("noappledouble", null);
         //options.put("noapplexattr", null);
         options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_DEBUG, false);
         options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_READ_ONLY, false);
-
-        fuse = Fuse.getFuse();
-        fuse.mount(fs, mountPoint, options);
     }
 
-    /** */
-    private int exec(String... commandLine) throws Exception {
-        ProcessBuilder processBuilder = new ProcessBuilder(commandLine);
-        processBuilder.inheritIO();
-        Process process = processBuilder.start();
-        process.waitFor();
-        return process.exitValue();
-    }
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "vavi.net.fuse.javafs.JavaFSFuseProvider",
+        "vavi.net.fuse.jnrfuse.JnrFuseFuseProvider",
+        "vavi.net.fuse.fusejna.FuseJnaFuseProvider",
+    })
+    public void test01(String providerClassName) throws Exception {
+        System.setProperty("vavi.net.fuse.FuseProvider.class", providerClassName);
 
-    /**
-     * create
-     * write
-     * chmod
-     * chmod
-     * chmod
-     * flush
-     * lock
-     * release
-     */
-    @Test
-    public void testCopyFromLocalToTarget() throws Exception {
-        Path from = Paths.get("src/test/resources/Hello.java");
-        Path toDir = Paths.get(mountPoint, "VAVI_FUSE_TEST4");
-        if (!Files.exists(toDir)) {
-Debug.println("[mkdir] " + toDir);
-            assertEquals(0, exec("/bin/mkdir", toDir.toString()));
-        }
-        Path to = toDir.resolve(from.getFileName());
-        if (Files.exists(to)) {
-Debug.println("[rm] " + to);
-            assertEquals(0, exec("/bin/rm", to.toString()));
-        }
-Debug.println("[cp] " + from + " " + to);
-        assertEquals(0, exec("/bin/cp", from.toString(), to.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", to.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", from.toString()));
-        assertTrue(Files.exists(to));
-        assertEquals(Files.size(from), Files.size(to));
-        assertEquals(Checksum.getChecksum(from), Checksum.getChecksum(to));
-        Debug.println("[rm] " + to);
-        assertEquals(0, exec("/bin/rm", to.toString()));
-Debug.println("[rmdir] " + toDir);
-        assertEquals(0, exec("/bin/rmdir", toDir.toString()));
-        assertFalse(Files.exists(to));
-        assertFalse(Files.exists(toDir));
-    }
+        Base.testFuse(fs, mountPoint, options);
 
-    /**
-     * open
-     * read
-     * flush
-     * lock
-     * release
-     */
-    @Test
-    public void testCopyFromRemoteToTarget() throws Exception {
-        Path from = Paths.get("src/test/resources/Hello.java");
-        Path remoteDir = fs.getPath("/VAVI_FUSE_TEST4");
-        if (!Files.exists(remoteDir)) {
-Debug.println("[_mkdir] " + remoteDir);
-            Files.createDirectory(remoteDir);
-        }
-        Path remote = remoteDir.resolve(from.getFileName().toString());
-Debug.println("[_cp] " + from + " " + remote);
-        Files.copy(from, remote, StandardCopyOption.REPLACE_EXISTING);
-        assertTrue(Files.exists(remote));
-
-        Path toDir = Paths.get("tmp");
-        if (!Files.exists(toDir)) {
-Debug.println("[_mkdir] " + toDir);
-            Files.createDirectory(toDir);
-        }
-        Path fuseDir = Paths.get(mountPoint, remoteDir.getFileName().toString());
-        Path fuse = fuseDir.resolve(remote.getFileName().toString());
-        Path to = toDir.resolve(fuse.getFileName().toString());
-        if (Files.exists(to)) {
-Debug.println("[_rm] " + to);
-            Files.delete(to);
-        }
-Debug.println("[cp] " + fuse + " " + to);
-        assertEquals(0, exec("/bin/cp", fuse.toString(), to.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", fuseDir.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", to.toString()));
-        assertTrue(Files.exists(to));
-        assertEquals(Files.size(fuse), Files.size(to));
-        assertEquals(Checksum.getChecksum(fuse), Checksum.getChecksum(to));
-Debug.println("[rm] " + remote);
-        assertEquals(0, exec("/bin/rm", fuse.toString()));
-Debug.println("[rmdir] " + toDir);
-        assertEquals(0, exec("/bin/rmdir", fuseDir.toString()));
-        assertFalse(Files.exists(fuse));
-        assertFalse(Files.exists(fuseDir));
-    }
-
-    @AfterAll
-    public static void after() throws Exception {
-Debug.println("unmount: start");
-        fuse.close();
-Debug.println("unmount: done");
+        fs.close();
     }
 }
 
